98ae39765e4e0adaac3962b46550e6d9d539c377
Elia Pellegrino <elia@piramid.studio>
2022-01-21 10:00:59 +0100

    bottom colors

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,9 @@
       <div class="row align-items-center">
         <div class="col-md order-last order-md-first">© 2022 Piramid | P.IVA 11438050012</div>
         <div class="col-md d-flex justify-content-center">
-          <svg class="logo" id="logo_small" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 265.24 279.25"><polygon points="141.09 93.08 127.89 93.08 159.9 186.16 173.09 186.16 141.09 93.08"/><path d="M108.77,186.16H92.15V169.29h16.62Z"/></svg>
+          <a href="#intro">
+            <svg class="logo" id="logo_small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 265.24 279.25"><polygon points="141.09 93.08 127.89 93.08 159.9 186.16 173.09 186.16 141.09 93.08"/><path d="M108.77,186.16H92.15V169.29h16.62Z"/></svg>
+          </a>
         </div>
         <div class="col-md d-flex justify-content-center justify-content-md-end">
           <ul class="navbar-nav text-uppercase">
@@ -14,7 +16,13 @@
         </div>
       </div>
     </div>
-  </footer>
+    </footer>
+    <div class="colors row">
+      <div class="bg-primary col-3"></div>
+      <div class="bg-purple col-3"></div>
+      <div class="bg-yellow col-3"></div>
+      <div class="bg-pink col-3"></div>
+    </div>
 </template>
 
 <script>
@@ -40,4 +48,13 @@ footer
     a
       color: white
 
+.colors
+  & > div
+    height: 2px
+  .bg-purple
+    background-color: $purple
+  .bg-yellow
+    background-color: $yellow
+  .bg-pink 
+    background-color: $pink
 </style>
