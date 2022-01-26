5550e5112e1e879074d48b0c508d97fd3d10c7e5
Elia Pellegrino <elia@piramid.studio>
2022-01-26 18:08:12 +0100

    partners logos

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -7,8 +7,8 @@
       </div>
       <div class="row">
         <div class="offset-md-1 col-md-10">
-          <div class="row logos d-flex">
-            <div v-for="(partner, index) in data.list" :key="index" class="col-4 col-md-4">
+          <div class="row logos">
+            <div v-for="(partner, index) in data.list" :key="index" class="d-flex align-items-center col-4 col-md-4" :class="partner">
               <img :src="'/images/partners/'+partner+'.svg'" :alt="partner" :title="partner">
             </div>
           </div>
@@ -40,15 +40,19 @@ export default {
   .logos
     & > div
       margin-bottom: 30px 
+      min-height: 50px
+      @include media-breakpoint-up(md)
+        min-height: 80px
     img
       width: auto
       max-height: 30px
+      max-width: 150px
       margin: 0 auto
       display: block
       filter: grayscale(100%)
       transition: all .3s ease-in-out
       @include media-breakpoint-up(md)
-        max-height: 40px
+        max-height: 80px
       &:hover
         filter: grayscale(0%)
 </style>
